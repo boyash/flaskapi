80e13dbcde5485abd4a0eedb23e753320ca63c93
boyash <byash3564@gmail.com>
2018-07-30 00:31:17 +0530

    removed irrelevant files

--- api/api_final.py
+++ api/api_final.py
@@ -1,9 +1,9 @@
-import flask
-from flask import request, jsonify
-import sqlite3
+import flask #importing flask module
+from flask import request, jsonify #from the flask module we are importing request and jsonify sub modules
+import sqlite3 #we are importing sqlite 3 which will be used in the db further in the code
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app = flask.Flask(__name__) #initialising the app
+app.config["DEBUG"] = True #setting app config for debug to true to trace the issues during dev. Set it to false for production
 
 def dict_factory(cursor, row):
 	d = {}
@@ -11,14 +11,14 @@ def dict_factory(cursor, row):
 		d[col[0]] = row[idx]
 	return d
 
-@app.route('/', methods=['GET'])
-def home():
-	return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for Advanced reading of Data.</p>'''
+@app.route('/', methods=['GET']) #defining the root route of the app 
+def home(): #this func returns a html string which is displayed in the browser when we hit localhost:5000 and acts as the root route of the app
+	return '''<h1>Distant Reading Archive</h1> 
+<p>A prototype API for Advanced reading of Data.</p>''' #replace it with a template 
 
 
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
+@app.route('/api/v1/resources/books/all', methods=['GET']) #this is the api route url to fetch all the books from the sqlite3 db
+def api_all(): 
 	conn = sqlite3.connect('books.db')
 	conn.row_factory = dict_factory
 	cur = conn.cursor()
@@ -48,7 +48,7 @@ def api_filter():
 		to_filter.append(id)
 	if published:
 		query += ' published=? AND'
-	 	to_filter.append(published)
+		to_filter.append(published)
 	if author:
 	 	query += ' author=? AND'
 	 	to_filter.append(author)
@@ -64,5 +64,4 @@ def api_filter():
 	return jsonify(results)
 
 app.run()
-
 
